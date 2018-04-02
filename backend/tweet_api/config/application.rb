ad98fe76314fb1090d8e68da43fb5206f7b3c3ee
moniquestigter <mstigter17@hotmail.com>
2018-04-02 17:34:48 -0600

    incio de folders

--- backend/tweet_api/config/application.rb
+++ backend/tweet_api/config/application.rb
@@ -29,5 +29,18 @@ module TweetApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.action_dispatch.default_headers.merge!({
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => '*'
+    })
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do 
+          origins '*'
+          resource '*', :headers => :any, :methods => [:get, :post, 
+              :put, :delete, :options]
+      end
+  end
   end
 end
